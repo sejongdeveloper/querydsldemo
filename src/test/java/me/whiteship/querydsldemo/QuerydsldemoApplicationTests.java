65f89e2564526e2a01c62d925ae16f0ab9c69a44
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-18 22:52:54 +0900

    스프링 데이터 Common 10. QueryDSL 연동

---
+++ src/test/java/me/whiteship/querydsldemo/QuerydsldemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.whiteship.querydsldemo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class QuerydsldemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
